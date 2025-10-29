88176c2165c5ef149adb878b1a60b26b73dd29e6
João Sá <joao.vsaa90@gmail.com>
2025-10-29 09:36:36 -0300

    feat: testes unitarios para botao

--- tests/map.spec.ts
+++ tests/map.spec.ts
@@ -1,23 +1,31 @@
 import { test, expect } from '@playwright/test';
 
-test('Título carrega corretamente', async ({ page }) => {
+test('Botão Limpa Pesquisa Renderiza', async ({ page }) => {
     await page.goto('/');
-    await expect(page.locator('h3')).toContainText('Consulta de Unidades');
+    await expect(page.locator('button')).toContainText('Limpar Pesquisa');
 });
 
+test('Botão Limpa Pesquisa Funciona', async ({ page }) => {
+    await page.goto('/');
+    const input = page.locator('#search');
+    await input.fill('São Luís');
+    await page.locator('button').click();
+    await expect(input).toHaveValue('');
+});
 
 test('Verifica se o mapa é renderizado', async ({ page }) => {
-    await page.goto('http://localhost:3000'); // ou o endereço local da tua app
+    await page.goto('/');
     const map = page.locator('#map');
     await expect(map).toBeVisible();
 });
 
 test('Pesquisa de unidade funciona', async ({ page }) => {
-    await page.goto('http://localhost:3000');
+    await page.goto('/');
 
-    const input = page.locator('.search-input');
+    const input = page.locator('#search');
     await input.fill('Imperatriz');
 
+    // Aguarda popup aparecer
     const markerPopup = page.locator('.leaflet-popup-content');
     await expect(markerPopup).toContainText('SEBRAE/MA - Unidade Regional Imperatriz');
 });
